029a1bbe499694b822a3395a8ee1e20b43f22709
Weslen Rodolfo Sarceño Colindres <weslencolindres@MacBook-Air-de-Weslen.local>
2022-08-24 17:57:41 -0600

    actualisacion de depositos

--- uber-app/src/components/Main/Main.jsx
+++ uber-app/src/components/Main/Main.jsx
@@ -25,7 +25,6 @@ export const Main = () => {
                             <Navigate to="/solicitudes" />
                     } />
                 </Routes>
-                <AppRunDeposit/>
             </div>
         </>
     )
